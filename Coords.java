02f166048cf2b0b3b8cabd051820665cc6fe0ee3
Pieter Dewachter <pieter.dewachter@student.kuleuven.be>
2016-03-08 07:36:05 +0100

    Removed unnecessary generics, makes the code look cleaner

--- Coords.java
+++ Coords.java
@@ -1,21 +1,21 @@
 package algorithms;
 
-public class Coords<A, B> {
-	public A x;
-	public B y;
+public class Coords {
+	public int x;
+	public int y;
 	
-	public Coords(A x, B y) {
+	public Coords(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
 	
 	public int hashCode() {
-		return 41 * (41 * x.hashCode()) + y.hashCode();
+		return 41 * (41 * x + y);
 	}
 	
 	public boolean equals(Object obj) {
-		if(obj instanceof Coords<?, ?>) {
-			Coords<A, B> that = (Coords<A, B>) obj;
+		if(obj instanceof Coords) {
+			Coords that = (Coords) obj;
 			return (that.x == this.x && that.y == this.y);
 		}
 		return false;
